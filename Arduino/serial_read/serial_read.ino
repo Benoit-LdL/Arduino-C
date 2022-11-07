5cd8298a0b2c05444e726ae96f21148ac2df2011
Benoit LAGASSE DE LOCHT <benoit.lagassedelocht@legrand.com>
2022-11-07 09:53:21 +0100

    update serial read ex

--- Arduino/serial_read/serial_read.ino
+++ Arduino/serial_read/serial_read.ino
@@ -4,6 +4,10 @@ const int led_red     = 32;
 const int led_yellow  = 33;
 const int led_green   = 25;
 
+bool led_red_state    = false;
+bool led_yellow_state = false;
+bool led_green_state = false;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -21,21 +25,33 @@ void loop() {
   while (!Serial.available());
 
   String input = Serial.readString();
-  if (input == "red")
+  Serial.println(input);
+  if (input == "red\n")
   {
-    digitalWrite(led_red,HIGH);
+    led_red_state = !led_red_state;
+    UpdateLed(led_red,led_red_state);
   }
-  else if (input == "yellow")
+  else if (input == "yellow\n")
   {
-    digitalWrite(led_yellow,HIGH);
+    led_yellow_state = !led_yellow_state;
+    UpdateLed(led_yellow,led_yellow_state);
   }
-  else if (input == "green")
+  else if (input == "green\n")
   {
-    digitalWrite(led_green,HIGH);
+    led_green_state = !led_green_state;
+    UpdateLed(led_green,led_green_state);
   }
   else
-    Serial.println("Input not valid!");
+    Serial.println("Wrong input");
 
   delay(1000);
 
+}
+
+void UpdateLed(int ledPin,bool led_state)
+{
+  if (led_state == true)
+    digitalWrite(ledPin,HIGH);
+  else
+    digitalWrite(ledPin,LOW);
 }
